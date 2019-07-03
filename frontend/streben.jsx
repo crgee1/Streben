416a417a69c0ed4e6537809daeab231739882eb6
crgee1 <christopherrgee@gmail.com>
2019-07-03 16:27:40 -0700

    make it better

--- frontend/streben.jsx
+++ frontend/streben.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store'
 import Root from './componets/root'
-import {login, signUp} from './actions/session_actions'
 
 document.addEventListener('DOMContentLoaded', () => {
     let preloadedState = undefined;
