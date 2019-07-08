7639e5e4aab37c4c00c1c8c173e773de07f594a7
crgee1 <christopherrgee@gmail.com>
2019-07-07 22:21:39 -0700

    Fix CSS styling

--- frontend/componets/training/training.jsx
+++ frontend/componets/training/training.jsx
@@ -5,7 +5,12 @@ class Training extends React.Component {
 
   render() {
     return (
-      <div></div>
+      <div className='training-main'>
+        <header className='training-head'>
+          <h1>My Activities</h1>
+        </header>
+        <section className='activity-count'></section>
+      </div>
     )
   }
 }
