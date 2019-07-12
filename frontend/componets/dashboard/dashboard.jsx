ab7b3409aa0699051918e041537036f5b0045255
crgee1 <christopherrgee@gmail.com>
2019-07-12 09:44:10 -0700

    Add total stats

--- frontend/componets/dashboard/dashboard.jsx
+++ frontend/componets/dashboard/dashboard.jsx
@@ -10,43 +10,85 @@ class Dashboard extends React.Component {
     this.props.fetchWorkouts();
   }
 
+  displayTime(seconds) {
+    let hour = Math.floor(seconds / 3600);
+    let min = Math.floor((seconds % 3600) / 60);
+    let sec = seconds % 60;
+    if (sec < 10) sec = `0${sec}`;
+    if (hour >= 1 && min < 10) min = `0${min}`;
+    return hour >= 1 ? `${hour}:${min}:${sec}` : `${min}:${sec}`;
+  }
+
   render() {
     const { currentUser, users } = this.props;
-    const workouts = this.props.workouts
-      .filter(workout => workout.user_id === currentUser.id)
-        .map((workout, i) => (
-          <ActivityFeedItem
-            user={users[workout.user_id]}
-            workout={workout}
-            key={i}
-          />
-        ))
+    const workoutArr = this.props.workouts.filter(
+      workout => workout.user_id === currentUser.id
+    );
+    const workouts = workoutArr.map((workout, i) => (
+      <ActivityFeedItem
+        user={users[workout.user_id]}
+        workout={workout}
+        key={i}
+      />
+    ));
+
+    const totDistance = workoutArr.reduce(
+      (acc, cur) => acc + cur.distance, 0
+    );
+    const totElevation = workoutArr.reduce(
+      (acc, cur) => acc + cur.elevation, 0
+    );
+    const totDuration = workoutArr.reduce(
+      (acc, cur) => acc + cur.duration, 0
+    );
+
     return (
       <div className="dashboard-home">
-        <div className='personal-stats'>
-          <section className='profile-card'>
-            <section className='avatar-image'>
-              <h1>
-                  {this.props.currentUser.username[0]}
-              </h1>
+        <div className="personal-stats">
+          <section className="profile-card">
+            <section className="avatar-image">
+              <h1>{this.props.currentUser.username[0]}</h1>
             </section>
-            <section className='profile-main-text'>
-              <h1>
-                {currentUser.username}
-              </h1>
+            <section className="profile-main-text">
+              <h1>{currentUser.username}</h1>
             </section>
+            <div className="total-stats">
+              <section>
+                <label>Activities</label>
+                <h2>{workoutArr.length}</h2>
+              </section>
+              <section>
+                <label>Distance</label>
+                <h2>{totDistance} mi</h2>
+              </section>
+              <section>
+                <label>Elevation</label>
+                <h2>{totElevation} ft</h2>
+              </section>
+              <section>
+                <label>Time</label>
+                <h2>{this.displayTime(totDuration)}</h2>
+              </section>
+            </div>
           </section>
         </div>
-        <div className='activity-feed'>
-          <section className='banner'></section>
+        <div className="activity-feed">
+          <section className="banner" />
           {workouts}
         </div>
-        <div className='advertise'>
+        <div className="advertise">
           <section>
-            <a href="https://www.linkedin.com/in/christopher-gee-426527118" className='personal-adver'>Linkedin</a>
+            <a
+              href="https://www.linkedin.com/in/christopher-gee-426527118"
+              className="personal-adver"
+            >
+              Linkedin
+            </a>
           </section>
           <section>
-            <a href="https://github.com/crgee1" className='personal-adver'>GitHub</a>
+            <a href="https://github.com/crgee1" className="personal-adver">
+              GitHub
+            </a>
           </section>
         </div>
       </div>
