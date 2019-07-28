83a9bf42163fdb14d55bfa7fb9bdf3b283fbf329
crgee1 <christopherrgee@gmail.com>
2019-07-27 18:33:16 -0700

    Add footer layout to splash

---
+++ frontend/componets/footer/footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <footer className='footer'>
+      <div className='footer-content'>
+        <section className='logo-column'>
+          <h2 className='footer-logo'>STREBEN</h2>
+        </section>
+        <section className='follow-column'>
+          <h2>FOLLOW</h2>
+        </section>
+      </div>
+    </footer>
+
+  )
+}
+
+
--- frontend/componets/splash/splash.jsx
+++ frontend/componets/splash/splash.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Footer from '../footer/footer';
 
 export default () => {
     return (
         <div className="splash">
-            <h1>The #1 app for runners and cyclists</h1>
+            <div className="splash-content">
+                <h1>The #1 app for runners and cyclists</h1>
+                <img src={window.splashURL} />
+            </div>
+            <Footer/>
         </div>
+        
     )
 }
    
