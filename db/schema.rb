969ed7096057cd965beeb5361ece4f221a5be42d
crgee1 <christopherrgee@gmail.com>
2019-08-27 23:31:12 -0700

    Fix rails migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_26_075544) do
+ActiveRecord::Schema.define(version: 2019_08_26_033230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
