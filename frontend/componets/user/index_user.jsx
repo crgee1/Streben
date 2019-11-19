e3eab495cde5067e674364b6a56685e23a3fe940
cgee1 <christopherrgee@gmail.com>
2019-11-18 19:55:40 -0800

    Create working search functionality

--- frontend/componets/user/index_user.jsx
+++ frontend/componets/user/index_user.jsx
@@ -5,6 +5,12 @@ import ActivitiesFooter from '../footer/recent_activities_footer';
 class UsersIndex extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      searchKey: '',
+      athletes: []
+    }
+
+    this.handleSearch = this.handleSearch.bind(this);
   }
 
   componentDidMount() {
@@ -12,33 +18,60 @@ class UsersIndex extends React.Component {
       .then(() => this.props.fetchUsers());
   }
 
+  handleSearch() {
+    this.setState({ searchKey: document.getElementById('athlete-search').value }, () => {
+      const {searchKey} = this.state;
+      if (searchKey === '') {
+        this.setState({athletes: []});
+      } else {
+        let athletes = this.props.users.filter(user => 
+          user.username.toLowerCase().includes(searchKey.toLowerCase()))
+        this.setState({
+          athletes
+        });
+      }
+    });
+  }
+
+  updateSearchKey(e) {
+    this.setState({searchKey: e.target.value});
+  }
+
   render() {
-    const { users, follows, currentUser, createFollow, deleteFollow } = this.props;
+    const { follows, currentUser, createFollow, deleteFollow } = this.props;
+    const { searchKey, athletes } = this.state;
     const followsObject = {};
     follows.forEach(el => followsObject[el.userId] = el)
     const followees = follows.map(el => el.userId);
-    const usersList = users.map((user, i) => 
-      <IndexUserItem
-        currentUser={currentUser}
-        user={user}
-        followees={followees}
-        follows={followsObject}
-        createFollow={createFollow}
-        deleteFollow={deleteFollow}
-        key={i}
-        i={i}
-      />
+    let usersList = (athletes.length === 0 && searchKey !== '') ? <div className="no-athletes-found">No athletes with name <strong>{searchKey}</strong> found</div> : 
+      this.state.athletes.map((user, i) => 
+        <IndexUserItem
+          currentUser={currentUser}
+          user={user}
+          followees={followees}
+          follows={followsObject}
+          createFollow={createFollow}
+          deleteFollow={deleteFollow}
+          key={i}
+          i={i}
+        />
     );
     return (
-        <div>
-      <div className='userIndex-main'>
-        <header className='userIndex-header'>
-          <h1>Athletes</h1>
-        </header>
-        {usersList}
-      </div>
-      <ActivitiesFooter workouts={this.props.recentWorkouts} />
+      <div>
+        <div className='userIndex-main'>
+          <header className='userIndex-header'>
+            <h1>Athletes</h1>
+          </header>
+          <div className="searchbar" onSubmit={this.handleSearch}>
+            <form>
+              <input id="athlete-search" type="text" placeholder="Athlete Name"/> 
+              <input type="submit" value="Search"/>
+            </form>
+          </div>
+          {usersList}
         </div>
+        <ActivitiesFooter workouts={this.props.recentWorkouts} />
+      </div>
     )
   }
 }
